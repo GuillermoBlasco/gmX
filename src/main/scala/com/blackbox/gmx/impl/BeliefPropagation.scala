ce79a3ed99f79cb8a91c2ee332d06065757fff80
GuillermoBlasco <guillermo.blasco.jimenez@gmail.com>
2014-10-15 17:56:11 +0200

    Added main methods to support BP refactor

--- src/main/scala/com/blackbox/gmx/impl/BeliefPropagation.scala
+++ src/main/scala/com/blackbox/gmx/impl/BeliefPropagation.scala
@@ -13,16 +13,12 @@ object BeliefPropagation {
    * Additional structure for BP
    */
 
-  private class BPVertex (val factor: Factor, var deltas: Map[VertexId, Factor]) extends java.io.Serializable {
-    // multiplies deltas but the one coming from j cluster
-    def packDeltasFor(j:VertexId) : Factor = factor * deltas.aggregate[Factor](Factor.emptyFactor(1.0))(
-      { case (f, (id, d)) => if (id != j) f * d else f }, _ * _
-    )
-    def aggregate() : Factor = factor * deltas.values.aggregate[Factor](Factor.emptyFactor(1.0))(_ * _, _ * _)
+  private class BPVertex (val factor: Factor, val deltas: Factor) extends java.io.Serializable {
+
   }
   private object BPVertex {
-    def apply(factor:Factor) : BPVertex = apply(factor, Map[VertexId, Factor]())
-    def apply(factor:Factor, deltas:Map[VertexId, Factor]) : BPVertex = new BPVertex(factor, deltas)
+    def apply(factor:Factor) : BPVertex = apply(factor, Factor.constantFactor(factor.scope(), 1.0))
+    def apply(factor:Factor, deltas: Factor) : BPVertex = new BPVertex(factor, deltas)
   }
 
   /*
@@ -32,86 +28,62 @@ object BeliefPropagation {
   // complete the vertex with incoming messages.
   // each vertex is the original Factor and a mapping of vertex to factors that are the incoming deltas.
   // Vertex i have the factor phi_i and a mapping j -> delta(j->i) where j takes values over the N(j)
-  private def toBPGraph(graph: Graph[Factor, Set[Variable]]) : Graph[BPVertex, Set[Variable]] = graph.mapVertices[BPVertex]((id, f) => BPVertex(f))
-
-  // builds a new BP vertex with updated deltas
-  private def vertexProcess(id:VertexId, vertex: BPVertex, newDeltas: Map[VertexId, Factor]) : BPVertex = BPVertex(vertex.factor, newDeltas)
-
-  private def deltaAggregation(d1: Map[VertexId, Factor], d2: Map[VertexId, Factor]) : Map[VertexId, Factor] = d1 ++ d2
-
-  private def sumDeltaMessage(triplet: EdgeTriplet[BPVertex, Set[Variable]]) : Iterator[(VertexId, Map[VertexId, Factor])] =
-    Iterator((triplet.dstId, Map[VertexId, Factor](
-      // delta srcId -> dstId is src factor * deltas to srcId (but the one coming from dstId)
-      triplet.srcId -> triplet.srcAttr.packDeltasFor(triplet.dstId).marginal(triplet.attr).normalized()
-    )))
-
-  private def maxDeltaMessage(triplet: EdgeTriplet[BPVertex, Set[Variable]]) : Iterator[(VertexId, Map[VertexId, Factor])] =
-    Iterator((triplet.dstId, Map[VertexId, Factor](
-      // delta srcId -> dstId is src factor * deltas to srcId (but the one coming from dstId)
-      triplet.srcId -> triplet.srcAttr.packDeltasFor(triplet.dstId).maxMarginal(triplet.attr).normalized()
-    )))
-
-
-  /*
-   * Computes the error of each step
-   */
-  private def stopFunction(
-                            epsilon:Double
-                            )(
-                            deltas: RDD[((VertexId, Map[VertexId, Factor]),(VertexId, Map[VertexId, Factor]))]
-                            ) : Boolean = {
-    val error: Double = deltas.aggregate(0.0)(
-      (error, v) => {
-        assert(v._1._1 == v._2._1) // old and news are matched
-        val vertexId = v._1._1
-        var vertexError = 0.0
-        v._1._2.keys foreach((k) =>
-          {vertexError = vertexError + Factor.distance(v._1._2(k).normalized(), v._2._2(k).normalized()) }
-        )
-        vertexError
-      },
-      (e1, e2) => e1 + e2
-    )
-    println(s"error: $error at ${System.currentTimeMillis()}")
-    error < epsilon
+  private def toBPGraph
+    (graph: Graph[Factor, Set[Variable]])
+    : Graph[BPVertex, Factor] = {
+    graph.mapVertices[BPVertex]((id, f) => BPVertex(f)).mapEdges[Factor]((edge) => Factor.constantFactor(edge.attr, 1.0))
   }
+
+  private def toClusterGraph
+    (graph: Graph[BPVertex, Factor])
+    : Graph[Factor, Set[Variable]] = {
+    graph.mapVertices[Factor]((id, v) => v.factor * v.deltas).mapEdges[Set[Variable]]((edge) => edge.attr.scope())
+  }
+
+  private def reduceDeltas(d1: Factor, d2: Factor) : Factor = d1 * d2
+
+  private def sumProjection(f: Factor, s: Set[Variable]) : Factor = f.marginal(s)
+  private def maxProjection(f: Factor, s: Set[Variable]) : Factor = f.maxMarginal(s)
 
   /*
    * Core BP algorithm
    */
-  private def apply(
-      deltaMessage: (EdgeTriplet[BPVertex, Set[Variable]]) => Iterator[(VertexId, Map[VertexId, Factor])],
-      maxIterations : Int,
-      epsilon : Double
-    )(
-      graph : Graph[Factor, Set[Variable]]
-    ) : Graph[Factor, Set[Variable]] = {
+  private def apply
+    (projection: (Factor, Set[Variable]) => Factor,
+     maxIterations : Int,
+     epsilon : Double)
+    (graph : Graph[Factor, Set[Variable]])
+    : Graph[Factor, Set[Variable]] = {
 
-    val g = toBPGraph(graph).cache()
+    assert(maxIterations > 0)
+    assert(epsilon > 0.0)
 
-    //Initial empty message
-    val calibrated = Pregel[BPVertex, Set[Variable], Map[VertexId,Factor]](
-      g,
-      Map.empty[VertexId, Factor],
-      maxIterations,
-      EdgeDirection.Either,
-      stopFunction(epsilon)
-    )(
-      vertexProcess,
-      deltaMessage,
-      deltaAggregation
-    )
-    val output: Graph[Factor, Set[Variable]] = calibrated.mapVertices((id ,vertex) => vertex.aggregate()).cache()
-    /*
-    // uncache working graph
-    g.vertices.unpersist(blocking=false)
-    g.edges.unpersist(blocking=false)
-    // uncache calibrated working graph
-    calibrated.vertices.unpersist(blocking=false)
-    calibrated.vertices.unpersist(blocking=false)
-    */
-    output
+    val g: Graph[BPVertex, Factor] = toBPGraph(graph).cache()
+
+    var iteration: Int = 0
+    var iterationError: Double = Double.PositiveInfinity
+
+    do {
+
+      // TODO: iteration
+
+      // TODO: compute errors
+      iterationError = Double.PositiveInfinity
+      iteration += 1
+    } while (iteration < maxIterations && iterationError > epsilon)
+
+    val outputGraph = toClusterGraph(g).cache()
+    g.unpersistVertices()
+    g.edges.unpersist()
+    outputGraph
   }
-  def sum(maxIterations: Int, epsilon: Double)(graph : Graph[Factor, Set[Variable]]) : Graph[Factor, Set[Variable]] = apply(sumDeltaMessage, maxIterations, epsilon)(graph)
-  def max(maxIterations: Int, epsilon: Double)(graph : Graph[Factor, Set[Variable]]) : Graph[Factor, Set[Variable]] = apply(maxDeltaMessage, maxIterations, epsilon)(graph)
+
+  def sum
+    (maxIterations: Int,
+     epsilon: Double)
+    (graph : Graph[Factor, Set[Variable]]) : Graph[Factor, Set[Variable]] = apply(sumProjection, maxIterations, epsilon)(graph)
+  def max
+    (maxIterations: Int,
+     epsilon: Double)
+    (graph : Graph[Factor, Set[Variable]]) : Graph[Factor, Set[Variable]] = apply(maxProjection, maxIterations, epsilon)(graph)
 }
